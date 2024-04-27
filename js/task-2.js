2a68b524818c6791385f217490b30c5d5cbe6120
kauuo <bonamix@ukr.net>
2024-04-28 00:02:16 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,30 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector(".gallery");
+
+images.forEach((image) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("galleryItem");
+
+  const img = document.createElement("img");
+  img.src = image.url;
+  img.alt = image.alt;
+  img.width = 360;
+
+  galleryItem.append(img);
+  gallery.append(galleryItem);
+});
+
+gallery.style.display = "flex";
+gallery.style.flexWrap = "wrap";
+gallery.style.columnGap = "24px";
+gallery.style.rowGap = "48px";
+gallery.style.listStyle = "none";
+
+img.classList.add("img");
+img.style.display = "block";
+img.style.maxWidth = "100%";
