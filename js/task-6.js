618222b0b26331626a209c00760c83a907abd484
kauuo <bonamix@ukr.net>
2024-04-29 21:26:06 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+createBtn.addEventListener("click", createBoxes);
+
+function createBoxes(event) {
+  boxes.style.display = "flex";
+  boxes.style.flexWrap = "wrap";
+
+  let count = parseInt(input.value);
+  if (count > 0 && count < 101) {
+    for (let i = 0; i < count; i++) {
+      const newBox = document.createElement("div");
+      newBox.classList.add("new-box");
+      newBox.style.width = "30px";
+      newBox.style.height = "30px";
+      newBox.style.backgroundColor = getRandomHexColor();
+
+      boxes.appendChild(newBox);
+    }
+  }
+}
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function destroyBoxes(event) {
+  const removeBoxes = document.querySelectorAll(".new-box");
+  removeBoxes.forEach((box) => box.remove());
+}
