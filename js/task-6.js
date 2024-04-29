e5f30349d0831a365d3bee24e00469426ae323d1
kauuo <bonamix@ukr.net>
2024-04-29 22:12:22 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -14,14 +14,17 @@ createBtn.addEventListener("click", createBoxes);
 function createBoxes(event) {
   boxes.style.display = "flex";
   boxes.style.flexWrap = "wrap";
+  boxes.style.gap = "10px";
+
+
 
   let count = parseInt(input.value);
   if (count > 0 && count < 101) {
     for (let i = 0; i < count; i++) {
       const newBox = document.createElement("div");
       newBox.classList.add("new-box");
-      newBox.style.width = "30px";
-      newBox.style.height = "30px";
+      newBox.style.width = `${30 + i * 10}px`;
+      newBox.style.height = `${30 + i * 10}px`;
       newBox.style.backgroundColor = getRandomHexColor();
 
       boxes.appendChild(newBox);
