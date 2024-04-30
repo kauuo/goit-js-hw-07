c76c6649cb3ce3216b031c015a75dc932295a3d6
kauuo <bonamix@ukr.net>
2024-04-30 14:33:45 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -8,6 +8,7 @@ const boxes = document.querySelector("#boxes");
 const input = document.querySelector("input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
+const fragment = document.createDocumentFragment();
 
 createBtn.addEventListener("click", createBoxes);
 
@@ -18,8 +19,6 @@ function createBoxes(event) {
 
   let count = parseInt(input.value);
   if (count > 0 && count < 101) {
-    const fragment = document.createDocumentFragment(); // Створення фрагмента
-
     for (let i = 0; i < count; i++) {
       const newBox = document.createElement("div");
       newBox.classList.add("new-box");
@@ -27,13 +26,11 @@ function createBoxes(event) {
       newBox.style.height = `${30 + i * 10}px`;
       newBox.style.backgroundColor = getRandomHexColor();
 
-      fragment.appendChild(newBox); // Додавання боксу до фрагмента
+      fragment.appendChild(newBox);
     }
-
-    boxes.appendChild(fragment); // Додавання всіх боксів до DOM за одну операцію
+    boxes.append(fragment);
   }
 }
-
 
 destroyBtn.addEventListener("click", destroyBoxes);
 
